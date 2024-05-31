501188baca559c95d7225430da63140e1db810a8
vbox <you@example.com>
2024-05-31 20:47:03 +0300

    update

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cp .env.localhost .env
 cp ./apps/dashboard/.env.localhost ./apps/dashboard/.env
+cat ~/.santa-thx-keys >> ./apps/dashboard/.env
 cp -a ../monorepo/certs .
 
 ./run.sh yarn
