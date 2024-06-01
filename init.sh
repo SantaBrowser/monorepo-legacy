8becc337828f7c64af64e3399beee306a2979267
dar6k6night <dar_k_night@mail.ru>
2024-06-01 21:02:49 +0700

    update

--- init.sh
+++ init.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cp .env.localhost .env
-cp ./apps/dashboard/.env.localhost ./apps/dashboard/.env
-cat ~/.santa-thx-keys >> ./apps/dashboard/.env
+set -xe
+
+cp .env.example .env
+cp ./apps/dashboard/.env.example ./apps/dashboard/.env
+
 cp -a ../monorepo/certs .
 
 ./run.sh yarn
